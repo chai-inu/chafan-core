74d63ab2fe6d5cdea09a324bc541eff1d6b0a495
Chai <bark@bite.com>
2024-09-27 12:35:38 -0400

    set up python in flake

--- flake.nix
+++ flake.nix
@@ -2,22 +2,20 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
+  outputs = { self, nixpkgs, flake-utils }: {
+    devShell.x86_64-linux =
         let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
-        in
-        with pkgs;
-        {
-          devShells.default = mkShell {
-            buildInputs = [ rust-bin.stable.latest.default ];
-          };
-        }
-      );
+            pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in pkgs.mkShell {
+            buildInputs = [
+            pkgs.python312
+            pkgs.python312Packages.alembic
+            ];
+        };
+  };
+
+
+
+
 }
