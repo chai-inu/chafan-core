9a7ebf059a997056e660491b0f49e4a025fdff48
Chai <bark@bite.com>
2024-09-27 13:26:39 -0400

    keep adding nix

--- flake.nix
+++ flake.nix
@@ -10,10 +10,26 @@
         in pkgs.mkShell {
             buildInputs = [
             pkgs.python311
+#            pkgs.poetry
             pkgs.python311Packages.uvicorn
             pkgs.python311Packages.fastapi
+            pkgs.python311Packages.python-dotenv
+            pkgs.python311Packages.sentry-sdk
+            pkgs.python311Packages.slowapi
+            pkgs.python311Packages.shortuuid
+            pkgs.python311Packages.starlette
+            pkgs.python311Packages.sqlalchemy
+            pkgs.python311Packages.pytz
+            pkgs.python311Packages.python-jose
+            pkgs.python311Packages.passlib
+            pkgs.python311Packages.boto3
+            pkgs.python311Packages.sentry-sdk
+            pkgs.python311Packages.dramatiq
+
+
+            pkgs.python311Packages.pydantic
+            pkgs.python311Packages.email-validator
 #            pkgs.python312Packages.alembic
-#            pkgs.poetry
 #
 #
 #            pkgs.postgresql
